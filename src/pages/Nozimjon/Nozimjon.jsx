5f866df08ae5f0f56129ceaa5d726e7db68f2e65
Nozimjon <masteryt123@mail.ru>
2024-11-28 20:07:14 +0500

    fix bugs #1

---
+++ src/pages/Nozimjon/Nozimjon.jsx
@@ -0,0 +1,19 @@
+import './Nozimjon.css'
+
+export default function Nozimjon() {
+    return(
+        <>
+            <nav>
+                
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
